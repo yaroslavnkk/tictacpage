21a6fa68f473cd8af22f8513659cebd16c125884
Yaroslav Nik <yaroslavnik711@gmail.com>
2024-06-07 18:23:02 +0300

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import Header from './components/Header.jsx';
+import gameLogo from '../public/game-logo.png'
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+      <>
+        <h1>React Tic-Tac-Toe</h1>
+        <Header image={gameLogo}/>
+      </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header({image}){
+    return(
+        <header>
+            <img src={image}></img>
+            <h1>Tic-Tac-Toe</h1>
+        </header>
+    );
+}
