38e48e8a5ecef3efd3d068f3d8b186b5fd71a225
Yaroslav <yaroslav.nikolaienko.oi.2022@lpnu.ua>
2024-06-10 17:55:00 +0300

    removed unnecessary state

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import gameLogo from '../public/game-logo.png'
 import Player from './components/Player.jsx';
 import GameBoard from './components/GameBoard.jsx';
 import Log from './components/Log.jsx';
-import {useState} from "React";
+import {useState} from "react";
+
+function setActivePlayer(playerTurn){
+    let currentPlayer = 'X';
+    if(playerTurn.length > 0 && playerTurn[0].player === 'X'){
+        currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
   const [playerTurn, setPlayerTurn] = useState([]);
   function handleSelectSquare(rowIndex, columnIndex){
-      setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
       setPlayerTurn((previousTurn) => {
-          let currentPlayer = 'X';
-          if(previousTurn.length > 0 && previousTurn[0].player === 'X'){
-              currentPlayer = 'O';
-          }
-          const updatedTurn = [{ square : {row : rowIndex, col : columnIndex}, player : currentPlayer} , ...previousTurn];
-          return updatedTurn;
+          const currentPlayer = setActivePlayer(playerTurn);
+          return [{square: {row: rowIndex, col: columnIndex}, player: currentPlayer}, ...previousTurn];
   });
   }
   return (
@@ -25,8 +27,8 @@ function App() {
         <main>
             <div id="game-container">
                 <ol id="players" className="highlight-player">
-                    <Player initialName="Player 1" playerSymbol="X" isActive={activePlayer === 'X'} />
-                    <Player initialName="Player 2" playerSymbol="O" isActive={activePlayer === 'O'} />
+                    <Player initialName="Player 1" playerSymbol="X" isActive={setActivePlayer(playerTurn) === 'X'} />
+                    <Player initialName="Player 2" playerSymbol="O" isActive={setActivePlayer(playerTurn) === 'O'} />
                 </ol>
                 <GameBoard onSelectSquare={handleSelectSquare} turns = {playerTurn} />
             </div>
